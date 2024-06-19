8dc0de82319d06ec5746d1dbdba06888fcde15cb
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-06-19 13:51:51 +0000

    as

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+  * main - Prints the alphabet at reverse
+  *
+  * Return: Always (Success)
+  */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'z' ; n >= 'a' ; num--)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
