5f88135f2e05886c4c2694e04c02394a55fe69ca
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-06-19 13:25:44 +0000

    as

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  **/
+
+
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9' a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
