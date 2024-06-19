6956e9a9ca6a7c7a9bec316528f100cf1d86b3a0
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-06-19 12:15:26 +0000

    as

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - Prints some letters of alphabet
+  *
+  * Return: Always (Success)
+  */
+
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
