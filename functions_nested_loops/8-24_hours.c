2ec2ea7b753bcf072f9f644f382d93377ab176d5
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-06-20 11:16:31 +0000

    asssss

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -4,19 +4,19 @@
  * jack_bauer - Prints every minute of the day of
  * Jack Bauer, starting from 00:00 to 23:59
  */
-
 void jack_bauer(void)
 {
-	int min, h;
-	for (h = 0 ; h <= 23 ; h++)
+	int minute, hour;
+
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (min = 0 ; min <= 59 ; min++)
+		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((min / 10) + '0');
-			_putchar((min % 10) + '0');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
 	}
