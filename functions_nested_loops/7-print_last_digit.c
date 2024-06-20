a2a1323ab541ed1dc63c7c9bb129b45201b5460a
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-06-20 10:54:24 +0000

    asssss

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - bir sayının son basamağını yazdırır
+ * @n: kontrol edilecek sayı
+ *
+ * Return: sayının son basamağının değeri
+ */
+int print_last_digit(int n)
+{
+    int last_digit = n % 10;
+
+    if (last_digit < 0)
+        last_digit = -last_digit;
+
+    _putchar(last_digit + '0');
+
+    return (last_digit);
+}
