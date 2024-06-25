1435074dcbfc61b408abb991914a2dae91fc4a68
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-06-25 09:26:33 +0000

    as

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+* more_numbers - Print 10 times the numbers since 0 up to 14
+* Return: 10 times of the numbers since 0 up to 14
+*/
+
+
 void more_numbers(void)
 {
 	int i, j;
