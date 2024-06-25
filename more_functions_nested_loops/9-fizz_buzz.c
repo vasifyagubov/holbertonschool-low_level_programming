e214182ee14838f8b54442950e465deaeb880982
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-06-25 10:55:27 +0000

    as

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,14 +6,13 @@
 * Multiples of both 3 and 5 should print FizzBuzz.
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 100 ; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if ((i % 3 == 0) &&  (i % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
@@ -34,6 +33,6 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
