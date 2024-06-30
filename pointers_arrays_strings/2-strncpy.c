103e2ce46bb4f8bfb3fca31a3ff6fe4773265452
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-06-30 16:18:53 +0000

    as

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -22,28 +22,4 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[index] = '\0';
 	}
 	return (dest);
-#include "main.h"
-
-/**
- * *_strncpy - Write a function that copies a string, with number of bytes
- * *@dest:This is the output dest
- * *@src:this is the input source
- * *@n:this is the number of bytes to copy
- * *
- * *Return: Copied string with the number of bytes
- * */
-
-char *_strncpy(char *dest, char *src, int n)
-{
-	int index;
-
-	for (index = 0; index < n && src[index] != '\0' ; index++)
-	{
-			dest[index] = src[index];
-	}
-	for (; index < n; index++)
-	{
-			dest[index] = '\0';
-	}
-	return (dest);
-}}
+}
