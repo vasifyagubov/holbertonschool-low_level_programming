0fbd9fb80d92735a73258dd4f6157c4e5f34c3b3
vasifyagubov <vasif.yaqubov85@gamil.com>
2024-07-04 11:00:41 +0000

    as

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i == 0;
+	unsigned int i = 0;
 
-	for ( ; i < n ; i++)
+	for ( ; i < n; i++)
 		dest[i] = src[i];
 
 	return (dest);
 }
-
