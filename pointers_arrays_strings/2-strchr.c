87285a484c12118a529cd034ff3baa922cac6fa8
vasifyagubov <vasif.yaqubov85@gamil.com>
2024-07-04 11:16:32 +0000

    as

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+*_strchr - Returns a pointer to the first occurrence
+*of the character c in the string s, or NULL if the
+*character is not found
+*
+*@s:string targeted
+*@c:character targeted
+*
+*Return: returns pointer to first occcurence of c
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+
+		s++;
+
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
+
