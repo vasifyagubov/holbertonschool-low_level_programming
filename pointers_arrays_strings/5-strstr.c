aebb458d47611fca62d3b787a9a14d2058ead6e8
vasifyagubov <vasif.yaqubov85@gamil.com>
2024-07-04 12:46:52 +0000

    as

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -17,20 +17,20 @@ char *_strstr(char *haystack, char *needle)
 
 
 	if (*needle == 0)
-		return(haystack);
-	while(*haystack)
+		return (haystack);
+	while (*haystack)
 	{
 		i = 0;
 
 		if (haystack[i] == needle[i])
 		{
-			do{
-				if(needle[i+1] == '\0')
-					return(haystack);
+			do {
+				if (needle[i+1] == '\0')
+					return (haystack);
 				i++;
-			}while(haystack[i] == needle[i]);
+			} while (haystack[i] == needle[i]);
 		}
 		haystack++;
 	}
-	return('\0');
+	return ('\0');
 }
