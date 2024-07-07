d91d86e5e0843bb87a1b8ff69cbe00be926c272e
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-07 08:05:47 +0000

    hi

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_strlen_recursion - gives length of a string
+*@s: string to be measured
+*Return: length of the string
+*
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_re_recursion(s + 1);
+		_putchar(*s);
+	}
+}
