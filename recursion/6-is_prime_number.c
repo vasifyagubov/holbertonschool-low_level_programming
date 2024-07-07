649a763836202d7e3eecd082c9968ae43af758e1
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-07 09:25:56 +0000

    hi

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,15 +10,15 @@
 *Return: 1 if the input integer is a prime number, otherwise return 0
 */
 
-int is_prime_number(int n)
+int is_prime_number(int prime)
 {
-	if (n <= 1)
+	if (prime <= 1)
 		return (0);
-	if (n == 2)
+	if (prime == 2)
 		return (1);
-	if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0))
+	if ((prime % 2 == 0) || (prime % 3 == 0) || (prime % 5 == 0))
 		return (0);
-	else if (n % 9 == 0 || n % 7 == 0)
+	else if (prime % 9 == 0 || prime % 7 == 0)
 		return (0);
 	else
 		return (1);
