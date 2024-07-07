c734ee1c552319ae9160c3c3e88cc3c8c670efde
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-07 09:21:54 +0000

    hi

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -19,7 +19,7 @@ int is_prime_number(int n)
 		return (1);
 	if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0))
 		return (0);
-	else if ((n % 7 == 0) || (n % 9 == 0))
+	else if (n % 7 == 0 || n % 9 == 0)
 		return (0);
 	else 
 		return (1);
