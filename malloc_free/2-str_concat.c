cdf8ce751fedb69ac5c73c008e1f95f2e5f1aed4
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-10 10:21:25 +0000

    NOT

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,4 +11,33 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	unt i = 0, j = 0;
+	char *concat;
 
+	if (!s1)
+		s1 = "";
+
+	if (!s2)
+		s2 = "";
+
+	while (s1[i]) != '\0')
+		i++;
+
+	while (s2[i] != '\0')
+		j++;
+
+	concat = malloc((i + j + 1) * sizeof(char));
+
+	if (!concat)
+		return(NULL);
+
+	for (i = 0 ; s1[i] != '\0' ; i++)
+		concat[i] = s1[i];
+
+	for (j = o ; s2[j] != '\0' ; j++)
+		concat[i + j] = ss2[j];
+
+	concat[i + j] = '\0';
+
+	return (concat);
+}
