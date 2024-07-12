076024a654328d50d02e5edf06001b5ff2056fe0
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-12 09:10:52 +0000

    yeah buddy

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* *malloc_checked - allocate memory with malloc
+*@b:unsigned int type
+*Return: return pointer
+*
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *abc;
+
+	abc = malloc(b);
+	if (abc = NULL)
+		return (98);
+	i
+
+	return (abc);
+}
