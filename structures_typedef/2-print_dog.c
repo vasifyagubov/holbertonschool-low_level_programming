528d224a020c7b4ba03dd95a3c2f56fb7eecb8c9
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-13 13:19:36 +0000

    MY dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - Prints a struct dog
+ * @d: The pointer to the struct dog to print
+ *
+ * Description: If an element of d is NULL, prints (nil) instead of the element.
+ *              If d is NULL, prints nothing.
+ */
+
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+
+	else
+		printf("Owner: %s\n", d->owner);
+}
