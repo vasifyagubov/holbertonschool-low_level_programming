12aa835ea95c44fb3ce98711f2f0f79bfaa95db8
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-13 13:49:24 +0000

    MY dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <dog.h>
+#include "stdio.h"
+
+/**
+ * free_dog - Frees dogs
+ * @d: The pointer to the dog_t structure to free
+ */
+
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
