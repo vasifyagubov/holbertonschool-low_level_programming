1018d1bf8fad8fbc5512a6f0d2bc65e6c73457ce
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-16 12:26:10 +0000

    HI

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array == NULL || action == NULL){
 
-		return(NULL);
+		return;
 	}
 
 	for (size_t i = 0 ; i < size ; i++){
