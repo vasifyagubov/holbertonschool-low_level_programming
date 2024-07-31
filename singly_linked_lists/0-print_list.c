9076e4cac3e132ec1c749348a896feeacbd29cfd
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-31 09:48:52 +0000

    Coding

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,13 +1,14 @@
 #include "lists.h"
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * print_list - function that prints all the elements of a list_t list
  * @h: Const pointer of structure list_t
  * Return: Number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
 	unsigned int index = 0;
---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stting.h>
+#include <stdio.h>
+#include "lists.h"
+
+void free_list(list_t *head)
+{
+
