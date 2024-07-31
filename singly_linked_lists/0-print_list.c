78e8476ebe42444768eabc0c25e4fb8c8cb663d4
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-31 08:27:39 +0000

    Coding

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,21 +12,21 @@
 
 size_t print_list(const list_t *h)
 {
-	int i = 0;
-        while (h != NULL)
+	int node = 0;
+
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
-
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
-		i++;
+		node++;
 		h = h->next;
 	}
-	return (i);
-
+	return (node);
 }
+
