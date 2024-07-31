5cb34624d4e1f1c262c98f3d66dad73c00e3061d
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-31 08:21:01 +0000

    Coding

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,32 +1,33 @@
-#include "lists.h"
-#include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
 
 /**
- * print_list - function that prints all the elements of a list_t list
- * @h: Const pointer of structure list_t
- * Return: Number of nodes
+ * print_list - main
+ * @h: our clone of struct
+ * Return: count of node
  */
+
+
 size_t print_list(const list_t *h)
 {
-	insigned int node = 0;
+	int i = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)\n")
-		} else
-		{
-			printf("[%d] %s\n", h->len, h-str);
+			printf("[0] (nil)\n");
 		}
 
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		i++;
 		h = h->next;
-		node++;
 	}
-	return (node);
+	return (i);
 
 }
-
