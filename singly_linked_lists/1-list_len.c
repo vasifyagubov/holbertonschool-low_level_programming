3057fbca8e7c6f70a7ec2875a3a34d016e035aae
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-31 08:43:43 +0000

    Coding

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: header node of singly linked list
+ * Return: A number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int a = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
