58e41a2271a9f4e8794baadc4e4e248bb5326beb
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-08-01 09:54:17 +0000

    Betty vs Kernel

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -48,11 +48,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = current->next;
 	new_node->prev = current;
-
-	if (current->next != NULL)
-		current->next->prev = new_node;
-
+	current->next->prev = new_node;
 	current->next = new_node;
+
 	return (new_node);
 }
 
