b610fb278a6195366a7250618f1678d2fb7e93dd
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-08-01 09:50:44 +0000

    Betty vs Kernel

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	if (idx == 0)
-		return (add_dnoteit(h, n));
+		return (add_dnodeint(h, n));
 
 	current = *h;
 
