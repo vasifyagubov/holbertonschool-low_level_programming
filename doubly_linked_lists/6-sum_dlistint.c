cfc9a4cab5e1fb98e7d51344ec7c211299f86cfa
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-08-01 09:17:41 +0000

    Betty vs Kernel

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - Bir dlistint_t bağlı listesindeki tüm verilerin toplamını döndürür.
+ * @head: Listenin başına işaretçi.
+ *
+ * Return: Listenin tüm verilerinin toplamı veya liste boşsa 0.
+ */
+
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+
+	}
+
+	return (sum);
+}
+
