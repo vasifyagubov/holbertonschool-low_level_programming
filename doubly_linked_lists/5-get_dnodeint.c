566144d99e4cd415e11129eb478acdb2337609cb
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-08-01 08:56:16 +0000

    Betty vs Kernel

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * get_dnodeint_at_index - function that returns the nth node of a list.
+ * @head: node.
+ * @index: value.
+ * Return: new node
+ */
+
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *node;
+	unsiigned int i = 0;
+
+	node = head;
+
+	while (node != head)
+	{
+		if (i == index)
+		{
+			return (node);
+
+		}
+		node = node->next;
+		i++
+	}
+	return (NULL);
+
+
+}
+
